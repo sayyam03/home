c4dfa88ff3f97dd319fa9e488d7b358a102f341a
Sayyam03 <sayyam03@gmail.com>
2021-01-05 19:47:30 -0500

    Updates

---
+++ precache-manifest.322fdaa9bf893beef7bad688b4ee3fc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e43fd05eb182e394e71d2ac546c066",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a1d458f1d021de90eef6",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d458f1d021de90eef6",
+    "url": "/home/static/js/main.e36d22e8.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "c3ee0dc844575622260f5eb7876d7f95",
+    "url": "/home/static/media/resume.c3ee0dc8.pdf"
+  },
+  {
+    "revision": "2ecc1109514603e1de9c420017722768",
+    "url": "/home/static/media/sayyam.2ecc1109.jpg"
+  }
+]);
